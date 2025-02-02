d248090365690bbff6024fcaa0ddf439592c9391
Ranny <Ranny@DESKTOP-1IS5O4I>
2025-02-02 16:14:42 -0300

    jdbc

--- AmykCakes/src/connection/DbConnection.java
+++ AmykCakes/src/connection/DbConnection.java
@@ -1,5 +1,20 @@
 package connection;
 
-public class DbConnection {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class DbConnection {
+	private static final String URL = "jdbc:mysql://localhost:3306/amykcakes";
+    private static final String USER = "root";
+    private static final String PASSWORD = "Maria0206_";
+
+    public static Connection getConexao() {
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            System.out.println("Erro na conexão: " + e.getMessage());
+            return null;
+        }
+    }
 }
