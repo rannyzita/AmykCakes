b16cb8fd9e7e2c7dcca4c37cc50f509ba9dd3eaf
Ayanamy <annafelix448@gmail.com>
2025-02-02 16:53:24 -0300

    Menu de navegação concluido

---
+++ AmykCakes/src/ui/PersonalizarPedidoController.java
@@ -0,0 +1,53 @@
+package ui;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+
+public class PersonalizarPedidoController {
+
+    @FXML
+    private Label labelTelaInicial;
+    @FXML
+    private Label labelPersonalizarPedido;
+    @FXML
+    private Label labelMinhasEncomendas;
+    @FXML
+    private Label labelSobreNos;
+    @FXML
+    private Pane mainPane; // O local onde as telas serão trocadas
+
+    @FXML
+    public void initialize() {
+        labelTelaInicial.setOnMouseClicked(event -> trocarTela("TelaInicial.fxml"));
+        labelPersonalizarPedido.setOnMouseClicked(event -> System.out.println("Personalizar Pedido clicado!"));
+        labelMinhasEncomendas.setOnMouseClicked(event -> trocarTela("MinhasEncomendas.fxml"));
+        labelSobreNos.setOnMouseClicked(event -> trocarTela("SobreNos.fxml"));
+    }
+
+    public void trocarTela(String fxml) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(fxml));
+            Pane novaTela = loader.load();
+            
+            // Passa a referência do mainPane para o controlador da nova tela
+            Object controller = loader.getController();
+            if (controller instanceof TelaInicialController) {
+                ((TelaInicialController) controller).setMainPane(mainPane);
+            } else if (controller instanceof MinhasEncomendasController) {
+                ((MinhasEncomendasController) controller).setMainPane(mainPane);
+            } else if (controller instanceof SobreNosController) {
+                ((SobreNosController) controller).setMainPane(mainPane);
+            }
+            
+            mainPane.getChildren().setAll(novaTela);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void setMainPane(Pane mainPane) {
+        this.mainPane = mainPane;
+    }
+}
---
+++ AmykCakes/src/ui/SobreNosController.java
@@ -0,0 +1,53 @@
+package ui;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+
+public class SobreNosController {
+
+    @FXML
+    private Label labelTelaInicial;
+    @FXML
+    private Label labelPersonalizarPedido;
+    @FXML
+    private Label labelMinhasEncomendas;
+    @FXML
+    private Label labelSobreNos;
+    @FXML
+    private Pane mainPane; // O local onde as telas serão trocadas
+
+    @FXML
+    public void initialize() {
+        labelTelaInicial.setOnMouseClicked(event -> trocarTela("TelaInicial.fxml"));
+        labelPersonalizarPedido.setOnMouseClicked(event -> trocarTela("PersonalizarPedido.fxml"));
+        labelMinhasEncomendas.setOnMouseClicked(event -> trocarTela("MinhasEncomendas.fxml"));
+        labelSobreNos.setOnMouseClicked(event -> System.out.println("Sobre Nós clicado!"));
+    }
+
+    public void trocarTela(String fxml) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(fxml));
+            Pane novaTela = loader.load();
+            
+            // Passa a referência do mainPane para o controlador da nova tela
+            Object controller = loader.getController();
+            if (controller instanceof TelaInicialController) {
+                ((TelaInicialController) controller).setMainPane(mainPane);
+            } else if (controller instanceof MinhasEncomendasController) {
+                ((MinhasEncomendasController) controller).setMainPane(mainPane);
+            } else if (controller instanceof PersonalizarPedidoController) {
+                ((PersonalizarPedidoController) controller).setMainPane(mainPane);
+            }
+            
+            mainPane.getChildren().setAll(novaTela);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void setMainPane(Pane mainPane) {
+        this.mainPane = mainPane;
+    }
+}
