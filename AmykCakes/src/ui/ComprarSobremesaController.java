fb04833a8ba2bdd8fa5d360f1981a879b5e676e8
Ayanamy <annafelix448@gmail.com>
2025-02-04 11:02:02 -0300

    criação do controller do comprar sobremesa e criação da função de ir para

---
+++ AmykCakes/src/ui/ComprarSobremesaController.java
@@ -0,0 +1,13 @@
+package ui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class ComprarSobremesaController {
+	@FXML 
+	private TextField txtNome;
+    @FXML 
+    private TextField txtCobertura;
+    @FXML 
+    private TextField txtPreco;
+}
--- AmykCakes/src/ui/TelaInicialController.java
+++ AmykCakes/src/ui/TelaInicialController.java
@@ -2,12 +2,19 @@ package ui;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class TelaInicialController {
 
+	//Declação das funções do menu
+	
     @FXML
     private Label labelTelaInicial;
     @FXML
@@ -17,14 +24,36 @@ public class TelaInicialController {
     @FXML
     private Label labelSobreNos;
     @FXML
-    private Pane mainPane; // O local onde as telas serão trocadas
-
+    private Pane mainPane; // local onde as telas serão trocadas
+    
+    //Declaração das imagens de bolos da tela 
+    @FXML 
+    protected ImageView imgBolo1;
+    @FXML 
+    protected ImageView imgBolo2;
+    @FXML 
+    protected ImageView imgBolo3;
+    @FXML 
+    protected ImageView imgBolo4;
+    @FXML 
+    protected ImageView imgBolo5;
+    @FXML 
+    protected ImageView imgBolo6;
+    
     @FXML
     public void initialize() {
         labelTelaInicial.setOnMouseClicked(event -> System.out.println("Tela Inicial clicada!"));
         labelPersonalizarPedido.setOnMouseClicked(event -> trocarTela("PersonalizarPedido.fxml"));
         labelMinhasEncomendas.setOnMouseClicked(event -> trocarTela("MinhasEncomendas.fxml"));
         labelSobreNos.setOnMouseClicked(event -> trocarTela("SobreNos.fxml"));
+        
+        //Adiciona eventos de clique para cada bolo
+        imgBolo1.setOnMouseClicked(event -> onComprarBoloClick("Bolo de Chocolate"));
+        imgBolo2.setOnMouseClicked(event -> onComprarBoloClick("Bolo de Morango"));
+        imgBolo3.setOnMouseClicked(event -> onComprarBoloClick("Bolo de Cenoura"));
+        imgBolo4.setOnMouseClicked(event -> onComprarBoloClick("Bolo de Limão"));
+        imgBolo5.setOnMouseClicked(event -> onComprarBoloClick("Bolo de Nozes"));
+        imgBolo6.setOnMouseClicked(event -> onComprarBoloClick("Bolo de Maracujá"));
     }
 
     public void trocarTela(String fxml) {
@@ -44,6 +73,25 @@ public class TelaInicialController {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+    
+    @FXML
+    public void onComprarBoloClick(String nomeBolo) {
+    	try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/AmykCakes/src/ui/ComprarSobremesa.fxml"));
+            Parent root = loader.load();
+            
+            ComprarSobremesaController compraSobremesaController = loader.getController();
+            //(SETAR O NOME) compraSobremesaController.setProduto(nomeBolo);
+
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    	
+    	
     }
 
     public void setMainPane(Pane mainPane) {
