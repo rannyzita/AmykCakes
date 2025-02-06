9817886960eddbfddaabf92e337cff89a134348a
Ranny <Ranny@172.16.1.130>
2025-02-06 18:17:39 -0300

    so coisando o main

--- AmykCakes/src/main/Main.java
+++ AmykCakes/src/main/Main.java
@@ -1,12 +1,10 @@
+
 package main;
 
-import dao.RestricoesAlimentaresDAO;
-import model.Personalizacao;
-import model.RestricoesAlimentares;
+
 
 public class Main {
     public static void main(String[] args) {
-        
-       
+
     }
 }
